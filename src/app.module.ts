a52a6bf6e68089b26c630ad37919a6aa576eaf3d
Garv nanwani <garvwashere@gmail.com>
2023-02-04 12:21:33 +0530

    setup typeorm and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +10,11 @@ import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
