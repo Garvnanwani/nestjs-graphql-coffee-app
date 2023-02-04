d270ff8ce83316f206de73a1cc0d03a73dcaa568
Garv nanwani <garvwashere@gmail.com>
2023-02-04 18:20:09 +0530

    added dataloader to fix n+1 issue

--- src/coffees/coffee-flavor.resolver.ts
+++ src/coffees/coffee-flavor.resolver.ts
@@ -1,23 +1,14 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FlavorsByCoffeeLoader } from './data-loader/flavors-by-coffee.loader';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
 @Resolver(() => Coffee)
 export class CoffeeFlavorResolver {
-  constructor(
-    @InjectRepository(Flavor)
-    private readonly flavorsService: Repository<Flavor>,
-  ) {}
+  constructor(private readonly flavorsByCoffeeLoader: FlavorsByCoffeeLoader) {}
 
   @ResolveField('flavors', () => [Flavor])
   async getFlavorsOfCoffee(@Parent() coffee: Coffee) {
-    return this.flavorsService
-      .createQueryBuilder('flavor')
-      .innerJoin('flavor.coffees', 'coffee', 'coffee.id = :coffeeId', {
-        coffeeId: coffee.id,
-      })
-      .getMany();
+    return this.flavorsByCoffeeLoader.load(coffee.id);
   }
 }
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,9 +6,10 @@ import { CoffeesResolver } from './coffees.resolver';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
+import { FlavorsByCoffeeLoader } from './data-loader/flavors-by-coffee.loader';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor]), PubSubModule],
-  providers: [CoffeesResolver, CoffeesService, CoffeeFlavorResolver],
+  providers: [CoffeesResolver, CoffeesService, CoffeeFlavorResolver, FlavorsByCoffeeLoader],
 })
 export class CoffeesModule {}
