0074d53d5bff48ac4790e60e247c9664586ed8dd
Garv nanwani <garvwashere@gmail.com>
2023-02-04 15:55:34 +0530

    added union type for drinks

--- src/drinks/drinks.resolver.ts
+++ src/drinks/drinks.resolver.ts
@@ -1,20 +1,22 @@
 import { Query, Resolver } from '@nestjs/graphql';
-import { Drink } from 'src/common/interfaces/drink.interface';
+import { DrinksResultUnion } from 'src/common/unions/drinks-result.union';
 import { Coffee } from './../coffees/entities/coffee.entity';
 import { Tea } from './../teas/entities/tea.entity';
 
 @Resolver()
 export class DrinksResolver {
-  @Query(() => [Drink], { name: 'drinks' })
-  async findAll() {
-    const coffee = new Coffee();
-    coffee.id = 1;
-    coffee.name = 'Coffee';
-    coffee.brand = 'Nest';
+  @Query(() => [DrinksResultUnion], { name: 'drinks' })
+  async findAll(): Promise<typeof DrinksResultUnion[]> {
+    {
+      const coffee = new Coffee();
+      coffee.id = 1;
+      coffee.name = 'Coffee';
+      coffee.brand = 'Nest';
 
-    const tea = new Tea();
-    tea.name = 'Tea';
+      const tea = new Tea();
+      tea.name = 'Tea';
 
-    return [coffee, tea];
+      return [coffee, tea];
+    }
   }
 }
